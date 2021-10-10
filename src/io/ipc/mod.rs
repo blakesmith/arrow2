5006377e33dd1ee4158da85520db9c8b8f419c55
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-10-10 04:33:21 +0000

    Migrated to arrow_format crate.

--- src/io/ipc/mod.rs
+++ src/io/ipc/mod.rs
@@ -1,19 +1,10 @@
 //! APIs to read from and write to Arrow's IPC format.
 
-#![allow(missing_debug_implementations)]
-#![allow(non_camel_case_types)]
-#[allow(clippy::redundant_closure)]
-#[allow(clippy::needless_lifetimes)]
-#[allow(clippy::extra_unused_lifetimes)]
-#[allow(clippy::redundant_static_lifetimes)]
-#[allow(clippy::redundant_field_names)]
-pub mod gen;
-
 mod compression;
 mod convert;
 
 pub use convert::fb_to_schema;
-pub use gen::Message::root_as_message;
+pub use arrow_format::ipc::Message::root_as_message;
 pub mod read;
 pub mod write;
 
